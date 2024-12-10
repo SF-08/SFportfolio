734e410b04f0cb5fe39f87868eadaddb3db196db
SF-08 <shrgf1234@gmail.com>
2024-12-10 10:20:55 +0000

    Add files via upload

---
+++ Terminal/js/commands.js
@@ -0,0 +1,71 @@
+var twitter = 'https://twitter.com/sc0tt08';
+var instagram = 'https://www.instagram.com/sc0tt____/';
+var github = 'https://github.com/SF-08';
+var password = 'sfportfolio';
+var email = 'mailto: 24000681@midkent.ac.uk';
+var secretvideo = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+var cybersecurity = 'https://midkentcollege-my.sharepoint.com/:w:/g/personal/24000681_midkent_ac_uk/EdXzpP2vHZFOvZPfxfEbiG8BO46DqNAhClV6IXAmw_ixOg?e=RDGPFJ'
+var task1assessment = 'https://midkentcollege-my.sharepoint.com/:w:/r/personal/24000681_midkent_ac_uk/Documents/24-25%20IT/Unit%2013%20-%20Website%20Development%20(Project%201)/Task%201%20Assessment.docx?d=w947f646193684d03b44d60cbe2e50347&csf=1&web=1&e=8VqAdf'
+
+whois = [
+    "<br>",
+    "Hello, I'm Scott",
+    "I'm a student and an aspiring web developer currently learning at Midkent College at the Medway Campus",
+    "The subject I am currently learning is Level 2 IT & Cyber Security",
+    "Level 2 IT & Cyber Security is a 1 year course and can be learnt further on with the Level 3 course",
+    "This website has specifically been created to demonstrate what I currently know within coding as well as what I am currently",
+    "learning within college.",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+    "<br>",
+    'twitter        <a href="' + twitter + '" target="_blank">twitter/sc0tt08' + '</a>',
+    'instagram      <a href="' + instagram+ '" target="_blank">instagram/sc0tt___' + '</a>',
+    'github         <a href="' + github + '" target="_blank">github/SF-08' + '</a>',
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    'Click if Visitor        <a href="' + secretvideo + '" target="_blank">Click if Visitor' + '</a>',
+    "<br>"
+];
+
+projects= [
+    "<br>",
+    'My cybersecurity assessment.   <a href=' + cybersecurity + '"target="_blank">CSDF 1 Cybersecurity Assessment' + '</a>',
+    'My Task 1 assignment for Website Development.   <a href=' + task1assessment + '"target="_blank">Task 1 Assignment' + '</a>',
+    "<br>"
+]
+
+guidance = [
+    "<br>",
+    '<span class="command">whois</span>         Who is Scott?',
+    '<span class="command">whoami</span>        Who are you?',
+    '<span class="command">socials</span>       Present social networks',
+    '<span class="command">secret</span>        Very very secret..',
+    '<span class="command">projects</span>      View College Projects.',
+    '<span class="command">clear</span>         Clear the terminal',
+    '<span class="command">email</span>         For any enquires you may have.',
+    '<span class="command">banner</span>        Displays the banner.',
+    "<br>",
+];
+
+banner = [
+    '<span class="index">Scott Fuller All Knowledge reserved.</span>',                                                                           
+                                                                                  
+  "  _/_/_/                        _/      _/          _/_/_/_/            _/  _/                      ",
+  "  _/          _/_/_/    _/_/    _/_/_/_/_/_/_/_/      _/        _/    _/  _/  _/    _/_/    _/  _/_/ ",
+  "   _/_/    _/        _/    _/    _/      _/          _/_/_/    _/    _/  _/  _/  _/_/_/_/  _/_/",        
+  "      _/  _/        _/    _/    _/      _/          _/        _/    _/  _/  _/  _/        _/",        
+"_/_/_/      _/_/_/    _/_/        _/_/    _/_/      _/          _/_/_/  _/  _/    _/_/_/  _/",            
+'<span class="color2">Welcome to my Portfolio.</span>',
+"<span class=\"color2\">For a list of the avaliable commands, type</span> <span class=\"command\">'guidance'</span><span class=\"color2\">.</span>",
+];
---
+++ Terminal/js/main.js
@@ -0,0 +1,143 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+   else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("visitor@sfportfolio.com:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+// all commands
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "guidance":
+      loopLines(guidance, "color2 margin", 80);
+      break;
+    case "whois":
+      loopLines(whois, "color2 margin", 80);
+      break;
+    case "whoami":
+      loopLines(whoami, "color2 margin", 80);
+      break;
+    case "secret":
+      addLine("Oh no...", "color2", 80);
+      setTimeout(function() {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs');
+      }, 1000); 
+      break;
+    case "socials":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "email":
+      addLine('Opening mailto:<a href="mailto:24000681@midkent.ac.uk">24000681@midkent.ac.uk</a>...', "color2", 80);
+      newTab(email);
+      break;
+      case "clear":
+        setTimeout(function() {
+          terminal.innerHTML = '<a id="before"></a>';
+          before = document.getElementById("before");
+        }, 0.5);
+        break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+
+      
+    // socials
+    
+    case "twitter":
+      addLine("Opening Twitter...", "color2", 0);
+      newTab(twitter);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    case "github":
+      addLine("Opening Github...", "color2", 0);
+      newTab(github);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'guidance'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
